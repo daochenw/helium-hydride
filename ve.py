52a574a1dec3928884c0fbb135d7692d6c59d232
daochenw <wangdc@live.co.uk>
2017-11-11 00:59:48 +0000

    update to incorporate bond lengths easily; plot_disa ("plot disassociation") takes advantage of this capability and is able to plot the disassociation curve: using energies from the HF or UCC variational states, with the option to do so via repeated measurements (the number of which can be controlled) as in vqe, or via direct calculation.

--- ve.py
+++ ve.py
@@ -13,61 +13,65 @@ importlib.reload(post_scf)
 
 pauli = post_scf.pauli
 tsrp = post_scf.tsrp
-
-mat_new = post_scf.matrices(r=1.4632)
-h_tot = mat_new['h_tot']
-h_c = mat_new['h_c']
-tt = mat_new['tt']
+mat_new, h_tot, h_c, tt, operations, coef = None, None, None, None, None, None
 
 
-operations_ = ['ziii', 'izii', 'iizi', 'iiiz',
-               'zzii', 'iizz', 'izzi', 'ziiz',
-               'xzxi', 'yzyi', 'ixzx', 'iyzy',
-               'zxzx', 'zyzy', 'xzxz', 'yzyz',
-               'xixi', 'yiyi', 'ixix', 'iyiy',
-               'xxyy', 'yyxx', 'yxxy', 'xyyx']
+def mat(r):
+    global mat_new, h_tot, h_c, tt, operations, coef
+    # 1.4632
+    mat_new = post_scf.matrices(r=r)
+    h_tot = mat_new['h_tot']
+    h_c = mat_new['h_c']
+    tt = mat_new['tt']
 
-a1 = -0.5*h_c[0, 0]-0.25*(tt[0, 0, 0, 0]+tt[0, 0, 1, 1])
-a2 = -0.5*h_c[1, 1]-0.25*(tt[1, 1, 1, 1]+tt[0, 0, 1, 1])
-c1 = 0.5*h_c[0, 1]+0.25*(tt[0, 0, 0, 1]+tt[1, 1, 1, 0])
-d1 = -0.25*tt[0, 0, 0, 1]
-d2 = -0.25*tt[1, 1, 1, 0]
-f1 = -0.25*tt[0, 1, 0, 1]
-coef = [a1, a1, a2, a2,
-        0.25*tt[0, 0, 0, 0], 0.25*tt[1, 1, 1, 1], 0.25*tt[0, 0, 1, 1], 0.25*tt[0, 0, 1, 1],
-        c1, c1, c1, c1,
-        d1, d1, d2, d2,
-        d1, d1, d2, d2,
-        f1, f1, -f1, -f1]
+    operations_ = ['ziii', 'izii', 'iizi', 'iiiz',
+                   'zzii', 'iizz', 'izzi', 'ziiz',
+                   'xzxi', 'yzyi', 'ixzx', 'iyzy',
+                   'zxzx', 'zyzy', 'xzxz', 'yzyz',
+                   'xixi', 'yiyi', 'ixix', 'iyiy',
+                   'xxyy', 'yyxx', 'yxxy', 'xyyx']
 
-operations = [tsrp(operations_[i]) for i in range(24)]
+    a1 = -0.5*h_c[0, 0]-0.25*(tt[0, 0, 0, 0]+tt[0, 0, 1, 1])
+    a2 = -0.5*h_c[1, 1]-0.25*(tt[1, 1, 1, 1]+tt[0, 0, 1, 1])
+    c1 = 0.5*h_c[0, 1]+0.25*(tt[0, 0, 0, 1]+tt[1, 1, 1, 0])
+    d1 = -0.25*tt[0, 0, 0, 1]
+    d2 = -0.25*tt[1, 1, 1, 0]
+    f1 = -0.25*tt[0, 1, 0, 1]
+
+    coef = [a1, a1, a2, a2,
+            0.25*tt[0, 0, 0, 0], 0.25*tt[1, 1, 1, 1], 0.25*tt[0, 0, 1, 1], 0.25*tt[0, 0, 1, 1],
+            c1, c1, c1, c1,
+            d1, d1, d2, d2,
+            d1, d1, d2, d2,
+            f1, f1, -f1, -f1]
+
+    operations = [tsrp(operations_[i]) for i in range(24)]
 
 
 def plot_disa(r=100):
     # plots the g.s. energy for different bond length values
-    global mat_new, h_tot, h_c, tt
     r_range = np.linspace(0.5, 5, r)
     store = np.zeros([r_range.size, 3])
     n = 0
     for i in r_range:
-        mat_new = post_scf.matrices(r=i)
+        mat(i)
         print('The bond length is: ', i)
-        h_tot = mat_new['h_tot']
-        h_c = mat_new['h_c']
-        tt = mat_new['tt']
         store[n, 0] = i
-        res = optimise()
         t0 = np.zeros(3)
         za, zb = 2, 1
-        store[n, 1] = energy(t0) + za * zb / i
-        store[n, 2] = res.fun + za * zb / i
+        # store[n, 1] = var_e(t0, mode=0, n_repeat=10).fun + za * zb / i
+        # store[n, 2] = var_e(t0, mode=1, n_repeat=100).fun + za * zb / i
+        store[n, 1] = energy(t0, mode=0) + za * zb / i         # the HF energy is energy(t0)
+        # store[n, 2] = energy(t0, mode=1, n_repeat=1000) + za * zb / i
+        store[n, 2] = var_e(t0, mode=0).fun + za * zb / i
+
         n = n + 1
     plt.plot(store[:, 0], store[:, 1], label='Hartree-Fock')
     plt.plot(store[:, 0], store[:, 2], label='UCC variational')
     plt.xlabel('R (a.u.)')
     plt.ylabel('E (a.u.)')
     plt.title('Helium hydride ground state energy (E) as a function of bond length (R)')
-    plt.grid('--')
+    plt.grid(ls='--')
     plt.legend()
     return store
 
@@ -108,7 +112,7 @@ def prepare(t):
     return state
 
 
-def energy(t, mode=0):
+def energy(t, mode=1, n_repeat=10):
 
     state = prepare(t)
     true = np.dot(np.conj(state), np.matmul(h_tot, state))
@@ -116,7 +120,7 @@ def energy(t, mode=0):
     # observables have real expectations (Hermitian matrices have real e-vals)
 
     if mode == 1:
-        true = var_energy(state)
+        true = q_energy(state, n_repeat)
         # print('true energy is: ', true0)
         # print('mle energy is: ', true)
         # print('error is: ', np.abs((true-true0)))
@@ -128,11 +132,8 @@ def energy(t, mode=0):
     return true
 
 
-def var_energy(state):
+def q_energy(state, n_repeat=10):
 
-    eps = 1e-3
-    n = eps**(-2)
-    n = 100
     mle_op = np.zeros(24)
 
     def var_op(st, op, n_):
@@ -148,22 +149,24 @@ def var_energy(state):
         return smpl_
 
     for i in range(24):
-        smpl = var_op(state, operations[i], n)
-        mle_op[i] = 2*smpl/n-1
+        smpl = var_op(state, operations[i], n_repeat)
+        mle_op[i] = 2*smpl/n_repeat-1
 
     bias = h_c[0, 0] + h_c[1, 1] + 0.5*tt[0, 0, 1, 1] + 0.25*(tt[0, 0, 0, 0]+tt[1, 1, 1, 1])
+    # bias = 0
     mle_energy = np.dot(coef, mle_op) + bias
 
     return mle_energy
 
 
-def optimise():
-
+def var_e(t0=np.zeros(3), mode=0, n_repeat=10):
+    # t0=np.zeros(3) = start from the HF ground state
+    # could also use  t0 = np.ones(4) for another start state
     print('\nNow we optimise the energy over {FCI - (sz=0)} coefficients \n')
-    # start from the HF ground state
-    t0 = np.zeros(3)
-    # t0 = np.ones(4)
-    res = minimize(energy, t0, method='nelder-mead', options={'xtol': 1e-8, 'disp': True, 'maxiter': 1000})
+
+    def energy_(t): return energy(t, mode=mode, n_repeat=n_repeat)
+
+    res = minimize(energy_, t0, method='nelder-mead', options={'xtol': 1e-5, 'disp': True, 'maxiter': 1000})
 
     return res
 
@@ -231,10 +234,3 @@ def plot_3d(mode=0):
                            linewidth=0, antialiased=False)
 
     plt.show()
-
-
-
-
-
-
-
